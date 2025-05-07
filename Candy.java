24055feebd1c624675bdd48abf374f7048efae5b
cartierpackk <carterl8@nycstudents.net>
2025-05-07 09:26:14 -0400

    Create Candy.java

---
+++ Candy.java
@@ -0,0 +1,15 @@
+public class Candy {
+    private String flavor;
+
+    public Candy(String flavor) {
+        this.flavor = flavor;
+    }
+
+    /** Returns a String representing the flavor of this piece of candy */
+    public String getFlavor() {
+        return flavor;
+        }
+
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
